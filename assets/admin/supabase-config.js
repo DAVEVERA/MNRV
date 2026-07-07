814f17f1c86cac1c7f6dc23e16c56cf3f8cd69f1
Dave Vera <info@mnrv.nl>
2026-07-07 05:04:50 +0200

    cleaning up.

--- assets/admin/supabase-config.js
+++ assets/admin/supabase-config.js
@@ -1,6 +1,6 @@
 ﻿window.MNRV_SUPABASE_CONFIG = Object.freeze({
   url: 'https://jaecfljhejwcyxvkhmtx.supabase.co',
   key: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImphZWNmbGpoZWp3Y3l4dmtobXR4Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODAzMDU3MTYsImV4cCI6MjA5NTg4MTcxNn0.kWJGjRF0kbw5LM9dX2UKRPT5AonMgEGmSzxa2CF5M6g',
-  adminRedirectUrl: 'https://mnrv.nl/assets/chat-admin.html'
+  adminRedirectUrl: 'https://mnrv.nl/assets/admin/chat-admin.html'
 });
 
--- cyberspace-portal/cyberspace-portal/README.md
+++
@@ -1,7 +0,0 @@
-# Cyberspace Portal 🚪🌀
-
-A Pen created on CodePen.
-
-Original URL: [https://codepen.io/matt-cannon/pen/OPPJLNa](https://codepen.io/matt-cannon/pen/OPPJLNa).
-
-An interactive card featuring a mesmerizing glow effect that invites you to enter a portal. Once activated, you are unexpectedly sucked into the matrix, traveling through a digital tunnel filled with floating code snippets and particles, immersing you in a journey through cyberspace
